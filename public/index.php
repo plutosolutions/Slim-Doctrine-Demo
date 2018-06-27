9f4233518d9636613510363680efbdc31213b0ad
Marcel Hernandez <marcel.hernandez@edreamsodigeo.com>
2018-06-27 18:10:06 +0200

    attach WIP xhgui collector to app front controller

--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Slim\App;
 use Slim\Container;
 
+putenv('XHGUI_PROFILING=oyea');
+require_once __DIR__ . '/../vendor/perftools/xhgui-collector/external/header.php';
+
 /** @var Container $cnt */
 $cnt = require_once __DIR__ . '/../bootstrap.php';
 
